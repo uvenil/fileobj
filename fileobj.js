6176c50cb5d1c71934b47638f571c51574d9b394
uvenil <uvenil@web.de>
2018-05-11 12:41:48 +0200

    Fkt keyArr

--- fileobj.js
+++ fileobj.js
@@ -9,11 +9,11 @@ const aktFile = path.basename(__filename);
 console.log(`>--- ${aktFile} ---<`);
 const readDirAsync = promisify(readDir);
 const filePath = process.argv[2];
-const ordner = "/home/micha/Schreibtisch/moduleMA";
+const ord = "/home/micha/Schreibtisch/moduleMA";
 const exclOrdner = ["node_modules"];  // nicht verwendete Ordner 
 const inclDateien = ["package.json"]; // gesuchte Dateinamen
 
-const flist = async (dir) => { // liest alle Dateien inkl. Pfade aus dem Ordner dir und seinen Unterordnern 
+const flist = async (dir) => { // liest alle Dateipfade aus dem Ordner dir und seinen Unterordnern 
   try {
     const fileList = await readDirAsync(dir);
     return fileList;
@@ -54,23 +54,41 @@ const readArr = async (fileArr) => {  // liest JSON-Objekt-Array aus Dateipfad-A
     console.error('error', error);
   }
 };
-flist(ordner).then((fl) => { // fl = filelist
-  let filtEx = filterEx(fl, exclOrdner);
-  let filtIn = filterIn(filtEx, inclDateien);
-  return readArr(filtIn);
-}).then((objArr) => {
-  // console.log(filtIn);
-  console.log(objArr[2]);
-}).catch((error) => {
-  console.error('error', error);
-});
-  
+const keyArr = (arr, keys) => {  // erstellt aus einem Array und ein Attribut-Array ein Objekt
+  let obj = {};
+  let i = 0;
+  keys.forEach((key) => {
+    obj[new String(key)] = arr[i++] || null;
+  });
+  if (arr.length>keys.length) {
+    obj["restarray"] = arr.slice(keys.length, arr.length);
+  }  
+  return obj;
+};
+const jsonAusOrdner = async (ordner = ord) => {  // bestimmte json-Dateien aus ordner und Unterordnern herauslesen 
+  flist(ordner).then((fl) => { // fl = filelist
+    let filtEx = filterEx(fl, exclOrdner);
+    let filtIn = filterIn(filtEx, inclDateien);
+    let indArr = filtIn.map((el) => el.replace(ordner, ""));
+    console.log(indArr);
+    
+    return readArr(filtIn);
+  }).then((objArr) => {
+    // console.log(filtIn);
+    console.log(objArr[2]);
+  }).catch((error) => {
+    console.error('error', error);
+  });
+};
+let a1 = [2];
+let k1 = [1,2];
+let res = keyArr(a1, k1);
+console.log(res);
+
+// jsonAusOrdner(ord);
   // filtIn.forEach((el) => {
   //   console.log(`${path.dirname(el)}: ${path.basename(el)},`);
   // })
 // .catch((err)=>console.log(err));
-
-
-// readArr();
 
 // await fs.writeJson('./pack1.json', { ...packObj, name: 'fs-extra', micha: 'lieb!' })
--- readdir-usage.js
+++
@@ -1,8 +0,0 @@
-const walk = require('./readdir');
-
-const dir = "/home/micha/Schreibtisch/moduleMA";
-
-walk(dir, function(err, results) {
-  if (err) throw err;
-  console.log(results);
-});
