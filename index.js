695bb958afc41e3fa3db88ef77e04a685107d3d4
uvenil <uvenil@web.de>
2018-06-22 18:16:42 +0200

    index.js package.json erstmal fertig

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+// wichtigste Funktionen in module: jsoncsv, ObjPath, schnittstr, arrayfkt, objclone
+// jsoncsv analysiert json-Dateien im Ordner (Aufruf sh. package.json)
+// ObjPath erlaubt Objekt-Manipulationen
+
 // ToDo !!!: 
 // react Prototyp testen
 // mehr Bibliotheken einsetzen, npm Pakete analysieren
+// evtl. statische Fkt wie Object.isObject() von Object lösen
+// evtl. bessere Module packen, z.B. Fkt. sortObject aus ObjPath herausnehmen
 
 // efs-Dateien compilen
 // neue Funktionen in einen neuen Branch und eine eigene Funktion integrieren
